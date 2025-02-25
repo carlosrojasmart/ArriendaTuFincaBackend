b73999fed55c0838f94c4b5546fb7600d8d04a74
Alejandro Caicedo <alecaice@hotmail.com>
2025-02-25 17:30:21 -0500

    Creación incial del proyecto

---
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/ArriendatufincaApplication.java
@@ -0,0 +1,13 @@
+package com.arriendatufinca.arriendatufinca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArriendatufincaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArriendatufincaApplication.class, args);
+	}
+
+}
---
+++ arriendatufinca/src/test/java/com/arriendatufinca/arriendatufinca/ArriendatufincaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arriendatufinca.arriendatufinca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendatufincaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
