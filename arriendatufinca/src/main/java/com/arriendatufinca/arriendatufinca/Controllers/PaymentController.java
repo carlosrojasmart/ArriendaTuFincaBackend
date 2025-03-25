349113466fe03d2ff69c800494d9debdad892028
Juan Rodriguez <145948821+JuanRodR31@users.noreply.github.com>
2025-03-25 16:42:46 -0500

    Update PaymentController.java

--- arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Controllers/PaymentController.java
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Controllers/PaymentController.java
@@ -1,6 +1,6 @@
 package com.arriendatufinca.arriendatufinca.Controllers;
 
-import com.arriendatufinca.arriendatufinca.Entities.Payment;
+import com.arriendatufinca.arriendatufinca.DTO.PaymentDTO;
 import com.arriendatufinca.arriendatufinca.Services.Tenant.PaymentService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,13 +13,9 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
     private final PaymentService paymentService;
 
-    @PostMapping("/pay-rent")
-    public ResponseEntity<Payment> payRent(
-        @RequestParam Long rentalRequestId,
-        @RequestParam double amount,
-        @RequestParam String transactionId
-    ) {
-        Payment payment = paymentService.processPayment(rentalRequestId, amount, transactionId);
-        return ResponseEntity.ok(payment);
+    @PostMapping
+    public ResponseEntity<PaymentDTO> processPayment(@RequestBody PaymentDTO paymentDTO) {
+        PaymentDTO response = paymentService.processPayment(paymentDTO);
+        return ResponseEntity.ok(response);
     }
 }
