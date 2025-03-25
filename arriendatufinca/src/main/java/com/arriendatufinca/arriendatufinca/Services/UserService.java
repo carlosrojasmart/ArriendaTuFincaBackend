60a39b6f7065cc7c65f0fee7e9ddf2f7a58a99cc
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-24 21:27:34 -0500

    Update: Se implementaron las pruebas unitarias de usuario

--- arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Services/UserService.java
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Services/UserService.java
@@ -1,37 +1,39 @@
 package com.arriendatufinca.arriendatufinca.Services;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.arriendatufinca.arriendatufinca.DTO.UserDTO;
 import com.arriendatufinca.arriendatufinca.Entities.User;
+import com.arriendatufinca.arriendatufinca.Enums.StatusEnum;
 import com.arriendatufinca.arriendatufinca.Repositories.UserRepository;
 
+@Service
 public class UserService {
 
-    private UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ModelMapper modelMapper;
 
     public UserDTO get(Long id) {
-        return userRepository.findById(id).map(user -> {
-            UserDTO userDTO = new UserDTO();
-            userDTO.setId(user.getId());
-            userDTO.setUsername(user.getUsername());
-            userDTO.setName(user.getName());
-            userDTO.setLastName(user.getLastName());
-            userDTO.setEmail(user.getEmail());
-            return userDTO;
-        }).orElse(null);
+        Optional<User> userOptional = userRepository.findById(id);
+        UserDTO userDTO = new UserDTO();
+        if(userOptional != null) {
+            userDTO = modelMapper.map(userOptional.get(), UserDTO.class);
+        }
+        return userDTO;
     }
 
     public Iterable<UserDTO> getAll() {
-        return userRepository.findAll().stream().map(user -> {
-            UserDTO userDTO = new UserDTO();
-            userDTO.setId(user.getId());
-            userDTO.setUsername(user.getUsername());
-            userDTO.setName(user.getName());
-            userDTO.setLastName(user.getLastName());
-            userDTO.setEmail(user.getEmail());
-            return userDTO;
-        }).collect(Collectors.toList());
+        List<User> users = userRepository.findAll();
+        List<UserDTO> userDTOs = users.stream().map(user -> modelMapper.map(user, UserDTO.class)).collect(Collectors.toList());
+        return userDTOs;
     }
 
     public void delete(Long id) {
@@ -39,12 +41,10 @@ public class UserService {
     }
 
     public UserDTO save(UserDTO userDTO) {
-        User user = new User();
-        user.setUsername(userDTO.getUsername());
-        user.setName(userDTO.getName());
-        user.setLastName(userDTO.getLastName());
-        user.setEmail(userDTO.getEmail());
+        User user = modelMapper.map(userDTO, User.class);
+        user.setStatus(StatusEnum.ACTIVE);
         userRepository.save(user);
+        userDTO.setId(user.getId());
         return userDTO;
     }
 }
