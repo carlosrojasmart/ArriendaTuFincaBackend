fafb278e3b3f14e5ec35015736892099f7f0fa19
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-26 17:47:49 -0500

    Update: Se eliminaron imports no usados

--- arriendatufinca/src/test/java/com/arriendatufinca/arriendatufinca/Controllers/UserControllerTest.java
+++ arriendatufinca/src/test/java/com/arriendatufinca/arriendatufinca/Controllers/UserControllerTest.java
@@ -4,18 +4,13 @@ import com.arriendatufinca.arriendatufinca.DTO.UserDTO;
 import com.arriendatufinca.arriendatufinca.Services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
