87ff558bef49d141f4dfd07200b5045e421371d5
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2025-03-27 01:29:12 -0500

    arreglo a los metodos de property

--- arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Services/admin/PropertyAdminService.java
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Services/admin/PropertyAdminService.java
@@ -21,6 +21,7 @@ public class PropertyAdminService {
 
     @Autowired
     private PropertyRepository propertyRepository; 
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
